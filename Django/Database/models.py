c97018a04c22736bd9d2f648dd48db2028bfeb9d
Working Diagnosis <mfred@workingdiagnosis.com>
2025-06-23 18:03:08 -0400

    yippee

--- Django/Database/models.py
+++ Django/Database/models.py
@@ -8,7 +8,11 @@ class Client(models.Model):
     first = models.CharField(max_length=200)
     last = models.CharField(max_length=200)
     birthdate = models.DateField()
-    address = models.CharField(max_length=254)
+    street1 = models.CharField(max_length=254, default="")
+    street2 = models.CharField(max_length=254, default="")
+    city = models.CharField(max_length=254, default="")
+    state = models.CharField(max_length=2, default="")
+    zip = models.CharField(max_length=10, default="")
     phone = models.CharField(max_length=10)
     email = models.EmailField(max_length=254)
     vaccinated = models.BooleanField()
@@ -29,6 +33,8 @@ class Course(models.Model):
     saturday = models.BooleanField()
     sunday = models.BooleanField()
     instructor = models.CharField(max_length=200, blank=True)
+    resident_cost = models.DecimalField(decimal_places=2, max_digits=10, default=0.00)
+    non_resident_cost = models.DecimalField(decimal_places=2, max_digits=10, default=0.00)
 
     def __str__(self):
         return f"{self.name}"
